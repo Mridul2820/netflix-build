62cfe839f9c207a81eeacdce9f37cb7ffd40b4e4
Mridul2820 <immridulpanda2820@gmail.com>
2021-04-03 11:11:15 +0530

    app clean up and firebase set up

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBrflM7hcGRLUgWMajpntlVSBq-n5zfBB0",
+    authDomain: "app-builds-55eef.firebaseapp.com",
+    projectId: "app-builds-55eef",
+    storageBucket: "app-builds-55eef.appspot.com",
+    messagingSenderId: "1024975080340",
+    appId: "1:1024975080340:web:9025234d27229e23519d81",
+    measurementId: "G-J9Y9NF0GD7"
+};
