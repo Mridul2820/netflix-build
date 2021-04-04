091a6e67a3143a3e7d393256d6ed828a9d9d1cc3
Mridul2820 <immridulpanda2820@gmail.com>
2021-04-04 12:12:11 +0530

    react router install and setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React from 'react'
 import HomeScreen from './pages/HomeScreen';
 import './styles/app.scss';
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from "react-router-dom";
+
 const App = () => {
     return (
         <div className="app">
-            <HomeScreen />
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <HomeScreen />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     )
 }
