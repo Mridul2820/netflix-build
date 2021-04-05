8170f76c95e03939eacc4c507f57c3eff69658bd
Mridul2820 <immridulpanda2820@gmail.com>
2021-04-06 01:20:07 +0530

    adding redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import {
     BrowserRouter as Router,
     Switch,
     Route
 } from "react-router-dom";
 import { auth } from './firebase';
+import { login, logout, selectUser } from "./features/userSlice";
 
 import HomeScreen from './pages/HomeScreen';
 import LoginScreen from './pages/LoginScreen';
@@ -13,19 +15,25 @@ import './styles/app.scss';
 
 
 const App = () => {
-    const user = null
+    const user = useSelector(selectUser)
+    const dispatch = useDispatch()
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(userAuth => {
             if(userAuth) {
                 // logged in
-                console.log(userAuth)
+                dispatch(login({
+                    uid: userAuth.uid,
+                    email: userAuth.email,
+                }))
             }
             else {
                 // logged out
+                dispatch(logout)
             }
         })
         return unsubscribe
+        // eslint-disable-next-line
     }, [])
 
     return (
