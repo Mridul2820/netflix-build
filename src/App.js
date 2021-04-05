83306f64009ca3eb2162bd6a8f155dfb892ff612
Mridul2820 <immridulpanda2820@gmail.com>
2021-04-05 20:07:36 +0530

    sign in and register with email and password

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
     Route
 } from "react-router-dom";
+import { auth } from './firebase';
 
 import HomeScreen from './pages/HomeScreen';
 import LoginScreen from './pages/LoginScreen';
@@ -13,6 +14,20 @@ import './styles/app.scss';
 
 const App = () => {
     const user = null
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(userAuth => {
+            if(userAuth) {
+                // logged in
+                console.log(userAuth)
+            }
+            else {
+                // logged out
+            }
+        })
+        return unsubscribe
+    }, [])
+
     return (
         <div className="app">
             <Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyBrflM7hcGRLUgWMajpntlVSBq-n5zfBB0",
     authDomain: "app-builds-55eef.firebaseapp.com",
@@ -7,3 +9,10 @@ const firebaseConfig = {
     appId: "1:1024975080340:web:9025234d27229e23519d81",
     measurementId: "G-J9Y9NF0GD7"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { auth }
+export default db
